abf7adfc224805ae26836a519e456beeaae318bf
osamakhanedu <osamakhanedu@gmail.com>
2024-06-12 21:37:55 +0500

    customization

---
+++ customization.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+# Start message
+echo "Start customization"
+
+
+# Update package lists
+sudo apt update
+
+#install gnome shell extension manager
+sudo apt install gnome-shell-extension-manager
