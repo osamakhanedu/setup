bc38fab2b89c525e38d75f7045f9a2d7c24faf58
osamakhanedu <osamakhanedu@gmail.com>
2025-06-13 20:16:33 +0500

    improve added

--- install.sh
+++ install.sh
@@ -1,49 +1,30 @@
 #!/bin/bash
 
-
-# Start message
 echo "Installation started."
-
 
 # Update package lists
 sudo apt update
 
-# install curl and wget 
-#sudo apt install curl wget 
+# Install curl and wget
+sudo apt install -y curl wget
 
-# Install Zsh and Oh My Zsh
-#sudo apt install zsh
-#sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Install Zsh
+sudo apt install -y zsh
 
-# Make Zsh the default shell
-# chsh -s $(which zsh)
+# Install Oh My Zsh (unattended, so it doesn't switch shell)
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-
-# Install Insomnia REST Client from Snap
+# Install Snap packages
 sudo snap install postman
-
-
-# Install VLC from Snap
 sudo snap install vlc
-
-# Install IntelliJ IDEA Community Edition
 sudo snap install intellij-idea-community --classic
-
-
-#  Get this out of snap
 sudo snap install pinta
-
-# install dbeaver ce
 sudo snap install dbeaver-ce
-
 sudo snap install obsidian --classic
-
-# install brave 
 sudo snap install brave
-
-# install note taking app
 sudo snap install xournalpp
 
+# Set Zsh as the default shell (do this last)
+chsh -s $(which zsh)
 
-# You may need to log out and back in for changes to take effect
 echo "Installation completed. Please restart your system."
